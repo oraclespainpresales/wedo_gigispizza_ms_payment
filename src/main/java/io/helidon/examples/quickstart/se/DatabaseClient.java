e48fd7436a4e94acef7d3b9123e000dd3bc1f53c
fharris <fernando.harris@gmail.com>
2019-04-25 12:39:22 +0200

    baseline helidon payment microservice 1st commit

---
+++ src/main/java/io/helidon/examples/quickstart/se/DatabaseClient.java
@@ -0,0 +1,120 @@
+package io.helidon.examples.quickstart.se;
+
+import java.nio.file.*;
+import java.sql.*;
+import java.io.*;
+import java.util.*;
+
+import io.helidon.webserver.ServerResponse;
+
+
+/**
+* Executes SQL statements calls against a jdbc DB.
+* @version 1.00 2019-03-26
+* @author Fernando Harris
+* 
+*/
+public class DatabaseClient
+{
+ public String executecall(String paymentCode, String orderId, String paymentTime, String paymentMehtod, String serviceSurvey, String totalPayed, String customerId ) throws IOException
+ {
+	 String dbresult = "";
+	 
+    try
+    {
+    	dbresult = runTest(paymentCode, orderId, paymentTime, paymentMehtod, serviceSurvey, totalPayed, customerId );
+    }
+    catch (SQLException ex)
+    {
+       for (Throwable t : ex)
+          t.printStackTrace();
+    }
+    
+    return dbresult;
+ }
+
+ /**
+  * Runs a test by creating a table, adding a value, showing the table contents, and removing 
+  * the table.
+  */
+ public static String runTest(String paymentCd, String order, String payTime, String payMehtod, String servSurvey, String totPayed, String custId ) throws SQLException, IOException
+ {  
+	 String dbresult = "";
+	 
+    try (Connection conn = getConnectionNoFile();
+       Statement stat = conn.createStatement())
+    {
+    	
+    	//logging values passed:
+    	 System.out.println("parameter paymentCd: "+paymentCd);
+    	 System.out.println("parameter order: "+order);
+    	 System.out.println("parameter payTime: "+payTime);
+    	 System.out.println("parameter payMehtod: "+payMehtod);
+    	 System.out.println("parameter servSurvey: "+servSurvey);
+    	 System.out.println("parameter totPayed: "+totPayed);
+    	 System.out.println("parameter custId: "+custId);
+    	
+ 
+    	stat.executeUpdate("INSERT INTO PAYMENTS (PAYMENTCODE, ORDERID, PAYMENTTIME, PAYMENTMETHOD, SERVICESURVEY, TOTALPAYED, CUSTOMERID) "
+    			+ "VALUES ('"+paymentCd+"','"+order+"', "+payTime+", '"+payMehtod+"', '"+servSurvey+"', '"+totPayed+"', '"+custId+"')");
+    	
+
+       try (ResultSet result = stat.executeQuery("SELECT * FROM PAYMENTS where PAYMENTCODE = '"+paymentCd+"'"))
+       {
+          if (result.next()){
+             System.out.println(result.getString(1));
+             dbresult = result.getString(1);
+          } else {
+        	  System.out.println("ERROR IN DB");
+        	  dbresult = "ERROR IN DB";
+          }
+       }
+   
+    }
+    return dbresult;
+ }
+
+ 
+ /**
+  * Gets a connection from the properties specified in the code.
+  * @return the database connection
+  */
+ public static Connection getConnectionNoFile() throws SQLException, IOException
+ { 
+ 
+ 
+    String url = "jdbc:oracle:thin:@//130.61.124.136:1521/dodbhp_pdb1.sub03010825490.devopsvcn.oraclevcn.com";
+    String username = "microservice";
+    String password = "AAZZ__welcomedevops123";
+    
+    
+
+    return DriverManager.getConnection(url, username, password);
+ }
+
+ /**
+  * Gets a connection from the properties specified in the file database.properties.
+  * @return the database connection
+  */
+ /*
+ public static Connection getConnection() throws SQLException, IOException
+ { 
+    Properties props = new Properties();
+    try (InputStream in = Files.newInputStream(Paths.get("database.properties")))
+    {
+       props.load(in);
+    }
+    String drivers = props.getProperty("jdbc.drivers");
+    if (drivers != null) System.setProperty("jdbc.drivers", drivers);
+    String url = props.getProperty("jdbc.url");
+    String username = props.getProperty("jdbc.username");
+    String password = props.getProperty("jdbc.password");
+    
+    
+
+    return DriverManager.getConnection(url, username, password);
+ }
+*/
+
+ 
+}
---
+++ src/main/java/io/helidon/examples/quickstart/se/PaymentService.java
@@ -0,0 +1,210 @@
+/*
+ * Copyright (c) 2018, 2019 Oracle and/or its affiliates. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.helidon.examples.quickstart.se;
+
+import java.io.IOException;
+import java.util.Collections;
+
+import javax.json.Json;
+import javax.json.JsonBuilderFactory;
+import javax.json.JsonObject;
+
+import io.helidon.common.http.Http;
+import io.helidon.config.Config;
+import io.helidon.webserver.Routing;
+import io.helidon.webserver.ServerRequest;
+import io.helidon.webserver.ServerResponse;
+import io.helidon.webserver.Service;
+
+/**
+ * A ervice to create payments in database. Examples:
+ *
+ * Get default greeting message:
+ * curl -X GET http://localhost:8080/greet
+ *
+ * Get greeting message for Joe:
+ * curl -X GET http://localhost:8080/greet/Joe
+ *
+ * Create payment in database
+ * curl -X POST -H "Content-Type: application/json" -d '{"paymentid": "019374432883", "paymentTime": "18-JAN-2019 11:50 AM","orderId": "000101", "paymentMethod": "VISA", "serviceSurvey": "5","totalPayed": "33", "customerId": "c002"}' http://localhost:9002/helidon/payment
+ *
+ * The message is returned as a JSON object
+ */
+
+public class PaymentService implements Service {
+
+    /**
+     * The config file is used only to provide info regarding the server
+     */
+    
+    private static final JsonBuilderFactory JSON = Json.createBuilderFactory(Collections.emptyMap());
+
+    PaymentService(Config config) {
+       // this.greeting = config.get("app.greeting").asString().orElse("Ciao");
+    }
+
+    /**
+     * A service registers itself by updating the routine rules.
+     * @param rules the routing rules.
+     */
+    @Override
+    public void update(Routing.Rules rules) {
+        rules
+            .post("/payment", this::updateDatabaseHandler);
+    }
+
+    
+    
+    /**
+     * Set the preparation to call json object param check, database call and response.
+     * @param request the server request
+     * @param response the server response
+     */
+    private void updateDatabaseHandler(ServerRequest request,
+                                       ServerResponse response) {
+        request.content().as(JsonObject.class).thenAccept(jo -> {
+			try {
+				updateJsonDBResponse(jo, response);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		});
+    }
+
+    /**
+     * Set the validaton of json object param check, database call and response.
+     * @param request the server request
+     * @param response the server response
+     */
+    private void updateJsonDBResponse(JsonObject jo, ServerResponse response) throws IOException {
+
+    	String errorParam;
+    	String dbresult;
+    	
+    	errorParam = "Generic error with json object";
+    	
+     
+   
+        if (!jo.containsKey("paymentid")) {
+        	
+        	errorParam = "No paymentid provided";
+        	
+        	  JsonObject jsonErrorObject = JSON.createObjectBuilder()
+                      .add("error", errorParam)
+                      .build();
+              response.status(Http.Status.BAD_REQUEST_400)
+                      .send(jsonErrorObject);
+              return;
+        }
+        
+        if (!jo.containsKey("paymentTime")) {
+        	
+        	errorParam = "No paymentTime provided";
+        	
+        	  JsonObject jsonErrorObject = JSON.createObjectBuilder()
+                      .add("error", errorParam)
+                      .build();
+              response.status(Http.Status.BAD_REQUEST_400)
+                      .send(jsonErrorObject);
+              return;
+        }
+        
+        
+        if (!jo.containsKey("orderId")) {
+        	errorParam = "No orderId provided";
+        	
+        	  JsonObject jsonErrorObject = JSON.createObjectBuilder()
+                      .add("error", errorParam)
+                      .build();
+              response.status(Http.Status.BAD_REQUEST_400)
+                      .send(jsonErrorObject);
+              return;
+        }
+        
+        if (!jo.containsKey("paymentMethod")) {
+        	errorParam = "No paymentMethod provided";
+        	
+        	  JsonObject jsonErrorObject = JSON.createObjectBuilder()
+                      .add("error", errorParam)
+                      .build();
+              response.status(Http.Status.BAD_REQUEST_400)
+                      .send(jsonErrorObject);
+              return;
+        }
+        
+        if (!jo.containsKey("serviceSurvey")) {
+        	errorParam = "No serviceSurvey provided";
+        	
+        	  JsonObject jsonErrorObject = JSON.createObjectBuilder()
+                      .add("error", errorParam)
+                      .build();
+              response.status(Http.Status.BAD_REQUEST_400)
+                      .send(jsonErrorObject);
+              return;
+        }
+        
+        if (!jo.containsKey("totalPayed")) {
+        	errorParam = "No totalPayed provided";
+        	  JsonObject jsonErrorObject = JSON.createObjectBuilder()
+                      .add("error", errorParam)
+                      .build();
+              response.status(Http.Status.BAD_REQUEST_400)
+                      .send(jsonErrorObject);
+              return;
+        }
+        
+        if (!jo.containsKey("customerId")) {
+        	errorParam = "No customerId provided";
+        	
+        	  JsonObject jsonErrorObject = JSON.createObjectBuilder()
+                      .add("error", errorParam)
+                      .build();
+              response.status(Http.Status.BAD_REQUEST_400)
+                      .send(jsonErrorObject);
+              return;
+        }
+                
+        
+ 
+      // Get parameters from json object  
+        String paymentid = jo.getString("paymentid");
+        String tempPaymentTime = jo.getString("paymentTime");
+        String paymentTime = "TO_DATE('"+tempPaymentTime+"', 'dd-mon-yyyy hh:mi PM')"; 
+        String orderId = jo.getString("orderId");
+    	String paymentMethod = jo.getString("paymentMethod");
+    	String serviceSurvey = jo.getString("serviceSurvey");
+    	String totalPayed = jo.getString("totalPayed");
+    	String customerId = jo.getString("customerId");
+        
+      
+    	// Call database
+        DatabaseClient dbclient = new DatabaseClient();
+    	dbresult = dbclient.executecall(paymentid, orderId, paymentTime, paymentMethod, serviceSurvey, totalPayed, customerId );
+        
+        //if dbresult is null or empty return a string with "SQL ERROR - Check logs"
+        JsonObject returnObject = JSON.createObjectBuilder()
+                .add("message", "payment creation requested")
+                .add("dbresult", dbresult)
+                .build();
+        
+        response.status(Http.Status.OK_200).send(returnObject);
+    }
+    
+
+
+}
