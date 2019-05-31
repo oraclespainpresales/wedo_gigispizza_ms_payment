de3a57f36b6f46abd5f6deb8237e1dcaaf2b8566
fharris <fharris@FHARRIS-ES.pt.oracle.com>
2019-05-31 15:16:56 +0200

    class body to get db results

--- src/main/java/io/helidon/examples/quickstart/se/DatabaseResult.java
+++ src/main/java/io/helidon/examples/quickstart/se/DatabaseResult.java
@@ -1,4 +1,53 @@
 package io.helidon.examples.quickstart.se;
 
+import java.util.ArrayList;
+
 public class DatabaseResult {
+
+        private ArrayList<String> lines = new ArrayList<String>();
+        private int numColumnsListArray;
+        private int linesLength;
+
+        private String[][] selectLine;
+        private int numColumnsSelectLine;
+        private int selectLineLength;
+
+
+
+
+    public DatabaseResult() {
+    }
+
+    public DatabaseResult(ArrayList<String> lines, String[][] selectLine, int numColumnsSelectLine, int numColumnsListArray, int linesLength, int selectLineLength) {
+        this.lines = lines;
+        this.selectLine = selectLine;
+        this.numColumnsSelectLine = numColumnsSelectLine;
+        this.numColumnsListArray = numColumnsListArray;
+        this.linesLength = linesLength;
+        this.selectLineLength = selectLineLength;
+    }
+
+    public String[][] getSelectLine() {
+        return selectLine;
+    }
+
+    public int getNumColumnsSelectLine() {
+        return numColumnsSelectLine;
+    }
+
+    public int getSelectLineLength() {
+        return selectLineLength;
+    }
+
+    public ArrayList<String> getLines() {
+        return lines;
+    }
+
+    public int getNumColumnsListArray() {
+        return numColumnsListArray;
+    }
+
+    public int getLinesLength() {
+        return linesLength;
+    }
 }
